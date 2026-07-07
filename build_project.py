755c301a75917a38f05b7ca234b444d791152b94
Jilanee Shaikh <jilaneeshaikh@gmail.com>
2026-07-07 13:22:10 +0530

    help v1 to v4

--- build_project.py
+++ build_project.py
@@ -9,6 +9,8 @@ from JNAS_AI_CORE.Builder.builder_v4 import main as v4_main
 
 
 if __name__ == "__main__":
-    if "--project" in sys.argv[1:]:
+    args = sys.argv[1:]
+    v4_flags = {"--project", "--provider", "--milestone", "--output"}
+    if not args or args[0].startswith("-") or any(arg in v4_flags for arg in args):
         sys.exit(v4_main())
     sys.exit(legacy_main())
